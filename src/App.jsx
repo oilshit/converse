7e394b18a054364129b5fa69d6458b8b0c9e5ce8
daimessdn@gmail.com <daimessdn@gmail.com>
2023-08-15 00:40:49 +0700

    added organization link

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { useState } from 'react'
 import { GlobalContext } from './context'
 import './App.css'
 
+import logo from "./assets/logo.png";
+
 function App() {
   const [unit, setUnit] = useState({
     conversion: "c-k",
@@ -30,13 +32,17 @@ function App() {
     <GlobalContext.Provider value={{ unit, setUnit, unitFrom, unitTo, changeUnitFrom, changeUnitTo }}>
       <main>
         <div id="title-container">
-          <div></div>
+          <div style={{ width: 30, height: 30 }}></div>
 
           <div className='app-title'>
             Unit Converter
           </div>
 
-          <div></div>
+          <div style={{ borderRadius: 2, overflow: "hidden", height: 30, }}>
+            <a href='https://github.com/oilshit' target='_blank' rel="noreferrer">
+              <img src={logo} width={30} height={30} alt='logo' />
+            </a>
+          </div>
         </div>
 
         <div className='form-unit'>
