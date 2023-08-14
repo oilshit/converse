158d7fb7f67a94fbc92220024c3b7d601f8f622a
daimessdn@gmail.com <daimessdn@gmail.com>
2023-08-15 00:30:08 +0700

    added physical and functional conversion for temperature

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,65 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { GlobalContext } from './context'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [unit, setUnit] = useState({
+    conversion: "c-k",
+    from: "Celcius",
+    to: "Kelvin",
+  });
+  const [unitFrom, changeUnitFrom] = useState("");
+  const [unitTo, changeUnitTo] = useState("");
+
+  const processUnitFrom = (value) => {
+    const conversedValue = value + 273.15;
+
+    changeUnitFrom(value);
+    changeUnitTo(conversedValue);
+  };
+
+  const processUnitTo = (value) => {
+    const conversedValue = value + (273.15 * -1);
+
+    changeUnitTo(value);
+    changeUnitFrom(conversedValue);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <GlobalContext.Provider value={{ unit, setUnit, unitFrom, unitTo, changeUnitFrom, changeUnitTo }}>
+      <main>
+        <div id="title-container">
+          <div></div>
+
+          <div className='app-title'>
+            Unit Converter
+          </div>
+
+          <div></div>
+        </div>
+
+        <div className='form-unit'>
+          {unit.from}
+          <input
+            type="number"
+            name="unitFrom"
+            id="unitFrom"
+            value={unitFrom}
+            onChange={e => processUnitFrom(Number(e.target.value))} />
+        </div>
+
+        <div className='form-unit'>
+          {unit.to}
+          <input
+            type="number"
+            name="unitTo"
+            id="unitTo"
+            value={unitTo}
+            onChange={e => processUnitTo(Number(e.target.value))} />
+        </div>
+      </main>
+    </GlobalContext.Provider>
   )
 }
 
